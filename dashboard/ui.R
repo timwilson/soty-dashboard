331e2de063256ef96af4e6e607bade056aa11337
Tim Wilson <tim@timwilson.info>
2019-05-21 18:04:26 -0500

    Add link to Performance Method article on archerytoolkit.net.

--- dashboard/ui.R
+++ dashboard/ui.R
@@ -19,8 +19,13 @@ shinyUI(fluidPage(
             
             hr(),
             p("The Performance Method is a project of the Minnesota Archers Alliance. Learn more about the
-              MAA and our programs at our", a(href="https://themnaa.org/", "web site"), "or on", 
-              a(href="www.facebook/com/MNArchersAlliance", "Facebook"), "."),
+              MAA and our programs at our", a(href="https://themnaa.org/", "web site"), "or", 
+              a(href="www.facebook/com/MNArchersAlliance", "Facebook"), "page."),
+            p("For more information about the Performance Method, read", 
+                em(a(href="http://archerytoolkit.net/soty/article.html", "The Performance Method: An Improved
+            Archer Ranking System for Determining a \"Shooter of the Year\"")), "by Tim Wilson. Please email or call with questions at", 
+                a(mailto="tim@themnaa.org", "tim@themnaa.org"),
+                " or (612) 599-5470."),
             img(src = "MAA_logo.png", width = "100%")
         ),
         
